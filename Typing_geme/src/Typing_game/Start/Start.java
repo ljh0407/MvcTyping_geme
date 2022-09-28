246b28162703cdbee57b235a24532b3f1c448d28
504 <504@DESKTOP-D4S2NMH>
2022-09-28 17:30:56 +0900

    9/28

--- Typing_geme/src/Typing_game/Start/Start.java
+++ Typing_geme/src/Typing_game/Start/Start.java
@@ -1,5 +1,13 @@
 package Typing_game.Start;
 
+import Typing_game.View.Mainpage;
+
 public class Start {
 
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
 }
--- Typing_geme/src/Typing_game/View/Mainpage.java
+++ Typing_geme/src/Typing_game/View/Mainpage.java
@@ -1,5 +1,12 @@
 package Typing_game.View;
 
+import java.util.Scanner;
+
 public class Mainpage {
 
+	Scanner scanner = new Scanner(System.in);
+	private static Mainpage mainp = new Mainpage();
+	private Mainpage() {}
+	private Mainpage getInstance() {return mainp;}
+	
 }
